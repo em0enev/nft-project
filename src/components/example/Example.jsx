cda7863a93c2a9fead3f36a0346158f8a650c99e
Emil Enev <emil.enev@abv.bg>
2021-10-12 21:51:30 +0300

    feat: add Logo component

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -3,16 +3,20 @@ import Container from "@mui/material/Container";
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
+import Logo from "../logo/logo";
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
-      <Container className={classNames(styles.container)} maxWidth="xl">
+      {/* <Container className={classNames(styles.container)} maxWidth="xl">
         <Paper className={classNames(styles.paper)}>
           <Link href="/about" color="secondary">
             Hi
           </Link>
         </Paper>
+      </Container>*/}
+      <Container className={classNames(styles.container)} >
+        <Logo type="muted"/>
       </Container>
     </div>
   );
