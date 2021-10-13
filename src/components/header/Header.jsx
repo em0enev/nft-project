31b8a406ed9ab5414ebebd6c94941d3692ab3485
Emil Enev <emil.enev@abv.bg>
2021-10-13 22:37:10 +0300

    fix: using grid system

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,24 +4,43 @@ import Logo from '../logo/logo';
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
 import InputAdornment from '@mui/material/InputAdornment';
-import OutlinedInput from '@mui/material/OutlinedInput';
 
 export default function Header() {
     return (
+        //     <Grid sx={{ borderTop: '2px dashed grey', padding: "10px", }} container direction="row" justifyContent="space-between" >
+        //         <Logo></Logo>
+        //         <TextField sx={{ width: "30%" }} placeholder="Find items, users and activities" variant="outlined" InputProps={{
+        //             startAdornment: (
+        //                 < InputAdornment position="start" >
+        //                     <SearchIcon color="primary" />
+        //                 </InputAdornment>
+        //             )
+        //         }} />
+        //         <div>
+        //             <Button variant='text'>Home</Button>
+        //             <Button variant='text'>Activity</Button>
+        //             <Button variant="contained">Explore</Button>
+        //         </div >
+        //     </Grid >
+
         <Grid sx={{ borderTop: '2px dashed grey', padding: "10px", }} container direction="row" justifyContent="space-between" >
-            <Logo></Logo>
-            <TextField sx={{ width: "30%" }} placeholder="Find items, users and activities" variant="outlined" InputProps={{
-                startAdornment: (
-                    < InputAdornment position="start" >
-                        <SearchIcon color="primary" />
-                    </InputAdornment>
-                )
-            }} />
-            <div>
+            <Grid item xs={2}>
+                <Logo></Logo>
+            </Grid>
+            <Grid item xs={5}>
+                <TextField fullWidth placeholder="Find items, users and activities" variant="outlined" InputProps={{
+                    startAdornment: (
+                        < InputAdornment position="start" >
+                            <SearchIcon color="primary" />
+                        </InputAdornment>
+                    )
+                }} />
+            </Grid>
+            <Grid item xs justifyContent="flex-end">
                 <Button variant='text'>Home</Button>
                 <Button variant='text'>Activity</Button>
                 <Button variant="contained">Explore</Button>
-            </div >
+            </Grid>
         </Grid >
     );
 }
