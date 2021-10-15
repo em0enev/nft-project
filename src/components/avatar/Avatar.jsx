101af1c63fda73a54aedda51d0c79fde9672bf14
Emil Enev <emil.enev@abv.bg>
2021-10-15 16:49:49 +0300

    fix: code refactoring in Avatar component

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -2,13 +2,14 @@ import classNames from "classnames";
 import styles from "./Avatar.module.scss"
 
 export default function Avatar({ size = 90, url, verified = false, }) {
-    const imgSrc = url !== undefined ? url : "./images/avatar.png";
+    const defaultImageSrc = "./images/avatar.png";
+    const badgeIconSrc = "./images/verified.svg";
+    const imgSrc = url !== undefined ? url : defaultImageSrc;
 
     return (
         <div className={classNames(styles.avatar)}>
-            <img  style={{ width:size, height:size }} className={classNames(styles.image)} src={imgSrc}></img>
-            {verified && <img className={classNames(styles.badge)} src="./images/verified.svg" alt="" />}
+            <img style={{ width: size, height: size }} className={classNames(styles.image)} src={imgSrc}></img>
+            {verified && <img className={classNames(styles.badge)} src={badgeIconSrc} />}
         </div>
     );
-
 }
