515a5c014af64ce8c741aef71eb799ef5f11e8e0
Emil Enev <emil.enev@abv.bg>
2021-10-26 21:38:44 +0300

    feat: add live auctions component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 
-import Card from '../src/components/card/Card.jsx';
+import Auctions from '../src/components/auctions/Auctions.jsx';
 
 export default function Index() {
-  return <Card name={"Clock"} user={{"avatarUrl":"images/avatar.png","verified":true}} mediaUrl={"images/nft.jpg"} price={200} currency={"BTC"} likes={20} />
+  return <Auctions cards={[{"name":"Clock","user":{"avatarUrl":"images/avatar.png","verified":true},"mediaUrl":"images/nft.jpg","price":200,"currency":"BTC","timeLeft":3600000},{"name":"DOGE","user":{"avatarUrl":"images/avatar.png","verified":true},"mediaUrl":"images/nft.jpg","price":200,"currency":"BTC","timeLeft":3600000},{"name":"BTC","user":{"avatarUrl":"images/avatar.png","verified":true},"mediaUrl":"images/nft.jpg","price":100,"currency":"BTC","timeLeft":3600000},{"name":"Litecoin","user":{"avatarUrl":"images/avatar.png","verified":true},"mediaUrl":"images/nft.jpg","price":300,"currency":"BTC","timeLeft":3600000}]} />
 }
