4a0b9f74eaa697a606612c80655c51b2f79a5094
Emil Enev <emil.enev@abv.bg>
2021-10-27 20:22:01 +0300

    feat: add Footer component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 
 import Auctions from '../src/components/auctions/Auctions.jsx';
+import Footer from '../src/components/footer/footer.jsx';
 
 export default function Index() {
-  return <Auctions cards={[{"name":"Clock","user":{"avatarUrl":"images/avatar.png","verified":true},"mediaUrl":"images/nft.jpg","price":200,"currency":"BTC","timeLeft":3600000},{"name":"DOGE","user":{"avatarUrl":"images/avatar.png","verified":true},"mediaUrl":"images/nft.jpg","price":200,"currency":"BTC","timeLeft":3600000},{"name":"BTC","user":{"avatarUrl":"images/avatar.png","verified":true},"mediaUrl":"images/nft.jpg","price":100,"currency":"BTC","timeLeft":3600000},{"name":"Litecoin","user":{"avatarUrl":"images/avatar.png","verified":true},"mediaUrl":"images/nft.jpg","price":300,"currency":"BTC","timeLeft":3600000}]} />
+  return <Footer />
 }
