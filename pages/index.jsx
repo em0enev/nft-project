24056397c3ccad2541a83b7e8a407ddc3a52eb0c
Emil Enev <emil.enev@abv.bg>
2021-11-10 23:33:35 +0200

    feat: add ProductActions component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 
-import ProductTabs from '../src/components/product/ProductTabs.jsx';
+import ProductActions from '../src/components/product/ProductActions.jsx';
 
 export default function Index() {
-  return <ProductTabs bids={[{"user":{"avatar":"/images/avatar.png","name":"hrisi","verified":true},"amount":30,"date":"2021-10-22T08:29:23.382Z"},{"user":{"avatar":"/images/avatar.png","name":"maxi","verified":true},"amount":1000,"date":"2021-10-22T08:29:23.382Z"}]} />
+  return <ProductActions isLive={true} currency={"ETH"} buyAmount={50} bidAmount={9} />
 }
