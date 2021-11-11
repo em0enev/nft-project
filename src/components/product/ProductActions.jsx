0081c28e02db7205ffeb17eb02a903538aed91c0
Emil Enev <emil.enev@abv.bg>
2021-11-11 12:53:56 +0200

    fix

--- src/components/product/ProductActions.jsx
+++ src/components/product/ProductActions.jsx
@@ -8,7 +8,7 @@ export default function ProductAction({ isLive, currency, buyAmount, bidAmount, 
         <div className={classNames(styles["product-action"])}>
             <Grid container spacing={1}>
                 <Grid item xs={7}>
-                    <Button disabled={isLive} className={classNames(styles.button)} variant="contained" onClick={onBuy}>Buy for {buyAmount} {currency}</Button>
+                    <Button disabled={isLive} className={classNames(styles.button)} variant="contained" onClick={onBuy} disabled={isLive}>Buy for {buyAmount} {currency}</Button>
                 </Grid>
                 <Grid item xs={5}>
                     <Button disabled={isLive} className={classNames(styles.button)} variant="outlined" color={"success"} sx={{ color: "rgba(36, 242, 94)", borderColor: "rgba(36, 242, 94)" }} onClick={onBid}>Place bid for {bidAmount} {currency}</Button>
