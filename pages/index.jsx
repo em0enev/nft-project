02296198555c6985b842b07a13ae5e53f252e5a8
Emil Enev <emil.enev@abv.bg>
2021-11-14 10:38:00 +0200

    feat: add ProfileHero component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 
-import Description from '../src/components/description/Description.jsx';
+import ProfileHero from '../src/components/profile/ProfileHero.jsx';
 
 export default function Index() {
-  return <Description text={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."} image={"https://nft-auction.herokuapp.com/uploads/0xa6dbe6b4f8e2905c26e123ec6fd08a8f7200dbc1_64120a76f4.jpg"} />
+  return <ProfileHero image={"https://nft-auction.herokuapp.com/uploads/0xa6dbe6b4f8e2905c26e123ec6fd08a8f7200dbc1_64120a76f4.jpg"} />
 }
