50d704cb59a4f311ea5379c6b8a99fc0a3c26c00
Emil Enev <emil.enev@abv.bg>
2021-11-16 12:50:48 +0200

    feat: add ActivityList

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 
-import ActivityListItem from '../src/components/activity/ActivityListItem.jsx';
+import ActivityList from '../src/components/activity/ActivityList.jsx';
 
 export default function Index() {
-  return <ActivityListItem created_at={"2021-10-22T08:29:23.382Z"} user={{"avatar":{"url":"/images/avatar.png"},"confirmed":false,"name":"Antonio Banderas"}} nft={{"name":"BTC","owner":{"username":"John Travolta","avatar":{"url":"/images/avatar.png"},"confirmed":true}}} type={"buy"} />
+  return <ActivityList items={[{"created_at":"2021-10-22T08:29:23.382Z","user":{"avatar":{"url":"/images/avatar.png"},"confirmed":false,"name":"Antonio Banderas"},"nft":{"name":"BTC","owner":{"username":"John Travolta","avatar":{"url":"/images/avatar.png"},"confirmed":true}},"type":"buy"},{"created_at":"2021-10-22T08:29:23.382Z","user":{"avatar":{"url":"/images/avatar.png"},"confirmed":false,"name":"Steven Seagal"},"nft":{"name":"BTC","owner":{"username":"John Wick","avatar":{"url":"/images/avatar.png"},"confirmed":true}},"type":"buy"}]} />
 }
