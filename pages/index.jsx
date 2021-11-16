4b34e761783a0a55a81ae0f4a7b3932ddfcadc4a
Emil Enev <emil.enev@abv.bg>
2021-11-16 21:50:20 +0200

    feat: add ExploreTitle component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 
-import ActivityList from '../src/components/activity/ActivityList.jsx';
+import ExploreTitle from '../src/components/explore/ExploreTitle.jsx';
 
 export default function Index() {
-  return <ActivityList items={[{"created_at":"2021-10-22T08:29:23.382Z","user":{"avatar":{"url":"/images/avatar.png"},"confirmed":false,"name":"Antonio Banderas"},"nft":{"name":"BTC","owner":{"username":"John Travolta","avatar":{"url":"/images/avatar.png"},"confirmed":true}},"type":"buy"},{"created_at":"2021-10-22T08:29:23.382Z","user":{"avatar":{"url":"/images/avatar.png"},"confirmed":false,"name":"Steven Seagal"},"nft":{"name":"BTC","owner":{"username":"John Wick","avatar":{"url":"/images/avatar.png"},"confirmed":true}},"type":"buy"}]} />
+  return <ExploreTitle text={"Explore"} />
 }
