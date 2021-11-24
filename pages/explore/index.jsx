3ec8b3f2a66defeca53ea4751e0a649a87ed2c76
Emil Enev <emil.enev@abv.bg>
2021-11-24 22:26:20 +0200

    fix: fix fetching routes

--- pages/explore/index.jsx
+++ pages/explore/index.jsx
@@ -30,55 +30,75 @@ export default function Explore() {
     }, [])
 
     useEffect(() => {
-        const types = {
-            1: "created_at",
-            2: "created_at",
-            3: "name",
-            4: "name",
-            5: "price",
-            6: "price"
+        async function fetchExploreData(path) {
+            const res = await fetch(`${process.env.apiUrl}${path}`);
+            if (res.status === 200) {
+                const data = await res.json();
+                setNfts(data.nfts)
+            }
         }
-        const sortProperty = types[sortByFilter];
-
-        function sortAsc(first, second) {
-            if (first[sortProperty] < second[sortProperty]) {
-                return 1;
-            }
-            if (first[sortProperty] > second[sortProperty]) {
-                return -1;
-            }
-            return 0;
+        if (sortByFilter !== 0 && priceFilterValue !== 0) {
+            fetchExploreData(`/explore?sort=${sortByFilter}&price=${priceFilterValue}`)
+        }
+        else if (sortByFilter !== 0) {
+            fetchExploreData(`/explore?sort=${sortByFilter}`);
+        } else if (priceFilterValue !== 0) {
+            fetchExploreData(`/explore?price=${priceFilterValue}`)
         }
 
-        function sortDsc(first, second) {
-            if (first[sortProperty] > second[sortProperty]) {
-                return 1;
-            }
-            if (first[sortProperty] < second[sortProperty]) {
-                return -1;
-            }
-            return 0;
-        }
 
-        const sortedNfts = nfts.sort((a, b) => sortByFilter % 2 !== 0 ? sortDsc(a, b) : sortAsc(a, b))
-        setNfts([...sortedNfts])
+    }, [sortByFilter, priceFilterValue])
 
-    }, [sortByFilter])
+    // useEffect(() => {
+    //     const types = {
+    //         1: "created_at",
+    //         2: "created_at",
+    //         3: "name",
+    //         4: "name",
+    //         5: "price",
+    //         6: "price"
+    //     }
+    //     const sortProperty = types[sortByFilter];
 
-    useEffect(() => {
-        const range = {
-            0: { min: Number.MIN_SAFE_INTEGER, max: Number.MAX_SAFE_INTEGER },
-            7: { min: 0, max: 0.01 },
-            8: { min: 0.01, max: 0.04 },
-            9: { min: 0.04, max: 1.2 }
-        }
+    //     function sortAsc(first, second) {
+    //         if (first[sortProperty] < second[sortProperty]) {
+    //             return 1;
+    //         }
+    //         if (first[sortProperty] > second[sortProperty]) {
+    //             return -1;
+    //         }
+    //         return 0;
+    //     }
 
-        const filterRange = range[priceFilterValue];
-        console.log(filterRange)
-        setMinSortValue(filterRange.min);
-        setMaxSortValue(filterRange.max)
+    //     function sortDsc(first, second) {
+    //         if (first[sortProperty] > second[sortProperty]) {
+    //             return 1;
+    //         }
+    //         if (first[sortProperty] < second[sortProperty]) {
+    //             return -1;
+    //         }
+    //         return 0;
+    //     }
 
-    }, [priceFilterValue])
+    //     const sortedNfts = nfts.sort((a, b) => sortByFilter % 2 !== 0 ? sortDsc(a, b) : sortAsc(a, b))
+    //     setNfts([...sortedNfts])
+
+    // }, [sortByFilter])
+
+    // useEffect(() => {
+    //     const range = {
+    //         0: { min: Number.MIN_SAFE_INTEGER, max: Number.MAX_SAFE_INTEGER },
+    //         7: { min: 0, max: 0.01 },
+    //         8: { min: 0.01, max: 0.04 },
+    //         9: { min: 0.04, max: 1.2 }
+    //     }
+
+    //     const filterRange = range[priceFilterValue];
+    //     console.log(filterRange)
+    //     setMinSortValue(filterRange.min);
+    //     setMaxSortValue(filterRange.max)
+
+    // }, [priceFilterValue])
 
     return (
         <div >
@@ -93,7 +113,7 @@ export default function Explore() {
                     </Grid>
                 </Grid>
                 <Grid container justifyContent="space-between" spacing={4} rowSpacing={2} maxWidth="xl" sx={{ marginTop: "60px" }}>
-                    {nfts && nfts.filter(nft => Number(nft.price) >= minSortValue && Number(nft.price) <= maxSortValue).map(el => {
+                    {nfts && nfts.map(el => {
                         return (
                             <Grid item xs={3} key={el.id}>
                                 <Card name={el.name} price={el.price} currency={el.currency} mediaUrl={el.source.url} user={el.owner} likes={el.likes} />
@@ -106,3 +126,5 @@ export default function Explore() {
         </div >
     )
 }
+
+//.filter(nft => Number(nft.price) >= minSortValue && Number(nft.price) <= maxSortValue)
--- pages/product/[id]/index.jsx
+++ pages/product/[id]/index.jsx
@@ -20,6 +20,7 @@ export default function index() {
             }
         }
     }, [id])
+
     return (
         <div>
             <Header></Header>
