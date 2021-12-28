f71fb9bc76c98d89b6ef39322ef10bb121ac53a3
Emil Enev <emil.enev@abv.bg>
2021-12-28 14:01:21 +0200

    fix: refactoring Footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,63 +1,50 @@
-import {
-    Grid,
-    Typography,
-    Button,
-    Container,
-    ButtonGroup,
-} from "@mui/material";
+import { Grid, Typography, Button } from "@mui/material";
 import Logo from "../logo/Logo.jsx";
 import styles from "./Footer.module.scss";
 import classNames from "classnames";
 
 export default function Footer() {
     return (
-        <div className={classNames(styles.divWrapper)}>
+        <div className={classNames(styles["divWrapper"])}>
             <Grid
                 container
                 maxWidth="xl"
-                direction="row"
+                className={classNames(styles["footer-container"])}
                 display={{ xs: "grid", md: "flex" }}
-                justifyContent={{ xs: "center", md: "space-between" }}
-                alignItems="center"
-                sx={{
-                    backgroundColor: "#181828",
-                    alignItems: "center",
-                    paddingX: "61px",
-                }}>
+                justifyContent={{ xs: "center", md: "space-between" }}>
                 <Grid
                     item
-                    display={{ xs: "none", md: "flex" }}
-                    sx={{ paddingY: "18px" }}>
+                    className={classNames(styles["logo"])}
+                    display={{ xs: "none", md: "flex" }}>
                     <Logo type={"muted"} />
                 </Grid>
                 <Grid item textAlign="center" order={{ xs: "2", md: "1" }}>
                     <Typography
                         color="textDisabled"
-                        className={classNames(styles.text)}>
+                        className={classNames(styles["text"])}>
                         Bum All Rights Reserved 2021
                     </Typography>
                 </Grid>
-                <Grid item order={{ xs: "1", md: "2" }}>
-                    <Button
-                        sx={{
-                            color: "#FFFFFF",
-                            padding: "0 10px 0 0",
-                            fontSize: "0.9rem",
-                            textTransform: "none",
-                        }}
-                        size="small">
-                        Privacy Policy
-                    </Button>
-                    <Button
-                        sx={{
-                            color: "#FFFFFF",
-                            padding: "0 0 0 10px",
-                            fontSize: "0.9rem",
-                            textTransform: "none",
-                        }}
-                        size="small">
-                        Cookie Policy
-                    </Button>
+                <Grid
+                    container
+                    item
+                    order={{ xs: "1", md: "2" }}
+                    xs={"auto"}
+                    spacing={2}>
+                    <Grid item>
+                        <Button
+                            className={classNames(styles["button"])}
+                            size="small">
+                            Privacy Policy
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            className={classNames(styles["button"])}
+                            size="small">
+                            Cookie Policy
+                        </Button>
+                    </Grid>
                 </Grid>
             </Grid>
         </div>
