de1e6289cf09cd56ec4aec403b1aaa423b2ffc1f
Emil Enev <emil.enev@abv.bg>
2021-12-29 20:55:41 +0200

    fix: add icons to like and live badges

--- src/components/product/ProductInfoLikes.jsx
+++ src/components/product/ProductInfoLikes.jsx
@@ -2,14 +2,21 @@ import { Chip } from "@mui/material";
 import classNames from "classnames";
 import styles from "./ProductInfoLikes.module.scss";
 import millify from "millify";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 export default function ProductInfoLikes({ amount = 0 }) {
     return (
         <div className={classNames(styles["product-info-likes"])}>
             <Chip
                 variant="outlined"
-                label={millify(amount)}
                 className={classNames(styles["likes"])}
+                icon={
+                    <FavoriteIcon
+                        className={classNames(styles["fav-icon"])}
+                        fontSize="small"
+                    />
+                }
+                label={millify(amount)}
             />
         </div>
     );
